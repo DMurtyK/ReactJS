66f6ea5e6540c2d5faa778b3365f3cf1386a40af
Deepa Murty Tekumalla <naradeepu@gmail.com>
2019-04-18 02:45:24 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Phone directory
+    <div>
+       {/*<div class = "header">
+         Phone Directory
       </div>
+      <button>Add</button>
+      <div>
+         <span>Name</span><br/>
+         <span>Phone</span>
+    </div>*/}
+    <label htmlFor = "name"> Name:</label>
+    <input id = "name" type= "text" placeholder="type here"  defaultValue = "Deepa"/>
+    </div> 
     );
+    
   }
 }
+
 
 export default App;
